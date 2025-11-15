b65b970b7d708258979204aa373f450b83d8c1f7
Mohit Patidar <mohitpatidar2212@gmail.com>
2025-11-15 16:11:50 +0530

    changes in allow origins

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React URL
+    allow_origins=["http://localhost:5173", "https://job-portal-pink-rho.vercel.app"],  # React URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
