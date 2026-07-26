f05c2e406b874c87d14ef24c122c9b10b28dc04c
Mohit Patidar <mohitpatidar2212@gmail.com>
2026-07-26 15:07:09 +0530

    Fix MAIL_PASSWORD assignment in mail.py
    Updated MAIL_PASSWORD assignment to ensure it uses SecretStr.

--- backend/mail.py
+++ backend/mail.py
@@ -23,11 +23,9 @@ settings = Settings(
 )
 
 
-# 💡 IMPORTANT FIX:
-# fastapi-mail requires MAIL_PASSWORD as SecretStr, NOT str
 conf = ConnectionConfig(
     MAIL_USERNAME=settings.MAIL_USERNAME,
-    MAIL_PASSWORD=settings.MAIL_PASSWORD,   # <--- SECRETSTR object  
+    MAIL_PASSWORD=settings.MAIL_PASSWORD,
     MAIL_FROM=settings.MAIL_FROM,
     MAIL_PORT=settings.MAIL_PORT,
     MAIL_SERVER=settings.MAIL_SERVER,
